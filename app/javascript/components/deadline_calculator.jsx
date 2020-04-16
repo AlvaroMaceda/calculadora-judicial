36354f45be1c1b8ca875fc1cc26a28d51af2e77b
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-04-16 23:15:08 +0200

    Dando formato al formulario

--- app/javascript/components/deadline_calculator.jsx
+++ app/javascript/components/deadline_calculator.jsx
@@ -4,59 +4,60 @@ import { useState } from 'react';
 import Autocomplete from "./autocomplete";
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
-import './deadline_calculator.css'
+import style from './deadline_calculator.module.scss'
 
-function DeadlineCalculator(){
+console.log(style)
 
-    const [startDate, setStartDate] = useState(new Date());
+function DeadlineCalculator() {
 
-    return (
-      <div className="row">
-        <div className="col-md-8 offset-md-2">
-          <div className="card card-outline-secondary">
+  const [startDate, setStartDate] = useState(new Date());
 
-            <div className="card-header">
-              <h3 className="mb-0">Calculadora de plazos judiciales</h3>
+  return (
+    <div className={style.container}>
+
+      <div className={style.card}>
+
+        <div className={style.header}>
+          <h3 className="mb-0">Calculadora de plazos judiciales</h3>
+        </div>
+
+        <div className={style.body}>
+          <form className="form" role="form" autoComplete="off" id="loginForm" noValidate="" method="POST" action="javascript:void(0);">
+
+            <div className="form-group">
+              <label htmlFor="municipality" className="lb-lg">Municipio</label>
+              <input type="text" className="form-control" name="municipality" id="municipality" required=""></input>
+              <div className="invalid-feedback">Por favor, introduzca un municipio correcto</div>
             </div>
 
-            <div className="card-body">
-              <form className="form" role="form" autoComplete="off" id="loginForm" noValidate="" method="POST" action="javascript:void(0);">
-
-                <div className="form-group">
-                  <label htmlFor="uname1" className="lb-lg">Municipio</label>
-                  <input type="text" className="form-control" name="uname1" id="uname1" required=""></input>
-                  <div className="invalid-feedback">Please enter your username or email</div>
+            <div className="form-group">
+              <label>Fecha de inicio</label>
+              <div className="row">
+                <div className="col-md-12">
+                  <DatePicker
+                    className="form-control col-md-12"
+                    dateFormat="dd/MM/yyyy"
+                    selected={startDate}
+                    onChange={date => setStartDate(date)}
+                  />
                 </div>
-
-                <div className="form-group">
-                  <label>Fecha de inicio</label>
-                  <div className="row">
-                    <div className="col-md-12">
-                      <DatePicker
-                        className="form-control col-md-12"
-                        dateFormat="dd/MM/yyyy"
-                        selected={startDate}
-                        onChange={date => setStartDate(date)}
-                      />
-                    </div>
-                  </div>
-                  <div className="invalid-feedback">Please enter a password</div>
-                </div>
-
-                <div className="form-group">
-                  <label>Días hábiles</label>
-                  <input type="password" className="form-control" id="pwd1" required="" autoComplete="new-password"></input>
-                  <div className="invalid-feedback">Por favor, introduzca un número</div>
-                </div>
-
-                <button type="submit" className="btn btn-success btn-lg float-right" id="btnLogin">Calcular</button>
-
-              </form>
+              </div>
+              <div className="invalid-feedback">Please enter a password</div>
             </div>
-          </div>
+
+            <div className="form-group">
+              <label>Días hábiles</label>
+              <input type="password" className="form-control" id="pwd1" required="" autoComplete="new-password"></input>
+              <div className="invalid-feedback">Por favor, introduzca un número</div>
+            </div>
+
+            <button type="submit" className="btn btn-success btn-lg float-right" id="btnLogin">Calcular</button>
+
+          </form>
         </div>
       </div>
-      );
+    </div>
+  );
 }
 
 export default DeadlineCalculator;
