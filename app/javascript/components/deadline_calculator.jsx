e41d617cac74f244fe47685c2e57f10139b7988a
Alvaro <alvaro@alvaronaceda.es>
2020-04-17 10:08:42 +0200

    Convertido el componente de la calculadora en stateful

--- app/javascript/components/deadline_calculator.jsx
+++ app/javascript/components/deadline_calculator.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Component } from 'react';
 import { useState } from 'react';
 
 import Autocomplete from "./autocomplete";
@@ -6,58 +7,122 @@ import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
 import style from './deadline_calculator.module.scss'
 
-console.log(style)
+// const [startDate, setStartDate] = useState(new Date());
+// const [municipality, setMunicipality] = useState('');
+// const [workDays, setworkDays] = useState(20);
 
-function DeadlineCalculator() {
+const handleSubmit = (event) => {
+  console.log(`
+    startDate: ${startDate}
+    municipality: ${municipality}
+    workDays: ${workDays}
+  `);
 
-  const [startDate, setStartDate] = useState(new Date());
+  event.preventDefault();
+}
 
-  return (
-    <div className={style.container}>
+// https://learnetto.com/blog/react-form-validation
+class DeadlineCalculator extends Component {
 
-      <div className={style.card}>
+  constructor (props) {
+    super(props);
+    this.state = {
+      startDate: new Date(),
+      municipality: '',
+      workDays: 0,
+      formErrors: {email: '', password: ''},
+      formValid: false
+    }
+  }
 
-        <div className={style.header}>
-          <h3 className="mb-0">Calculadora de plazos judiciales</h3>
-        </div>
+  setStartDate(value) {
+    this.setState({
+        ...this.state,
+        startDate: value
+      })
+  }
+  setMunicipality(value) {
+    this.setState({
+      ...this.state,
+      municipality: value
+    })
+  }
+  setworkDays(value) {
+    this.setState({
+      ...this.state,
+      workDays: value
+    })
+  }
 
-        <div className={style.body}>
-          <form className="form" role="form" autoComplete="off" id="loginForm" noValidate="" method="POST" action="javascript:void(0);">
+  handleSubmit(event){
+
+    event.preventDefault();
+    console.log('banana')
+
+    console.log(`
+      startDate: ${this.state.startDate}
+      municipality: ${this.state.municipality}
+      workDays: ${this.state.workDays}
+    `);
+  
+  }
+
+  render() {
+    return (
+      <div className={style.container}>
+
+        <div className={style.card}>
+
+          <div className={style.header}>
+            <h3 className="mb-0">Calculadora de plazos judiciales</h3>
+          </div>
+
+          <div className={style.body}>
+            <form className="form" role="form" autoComplete="off" id="loginForm" noValidate="" onSubmit={this.handleSubmit.bind(this)}>
 
             <div className="form-group">
-              <label htmlFor="municipality" className="lb-lg">Municipio</label>
-              <input type="text" className="form-control" name="municipality" id="municipality" required=""></input>
-              <div className="invalid-feedback">Por favor, introduzca un municipio correcto</div>
-            </div>
-
-            <div className="form-group">
-              <label>Fecha de inicio</label>
-              <div className="row">
-                <div className="col-md-12">
-                  <DatePicker
-                    className="form-control col-md-12"
-                    dateFormat="dd/MM/yyyy"
-                    selected={startDate}
-                    onChange={date => setStartDate(date)}
-                  />
+                <label>Fecha de inicio</label>
+                <div className="row">
+                  <div className="col-md-12">
+                    <DatePicker
+                      className="form-control col-md-12"
+                      dateFormat="dd/MM/yyyy"
+                      selected={this.state.startDate}
+                      onChange={date => this.setStartDate(date)}
+                    />
+                  </div>
                 </div>
+                <div className="invalid-feedback">Please enter a password</div>
               </div>
-              <div className="invalid-feedback">Please enter a password</div>
-            </div>
 
-            <div className="form-group">
-              <label>Días hábiles</label>
-              <input type="password" className="form-control" id="pwd1" required="" autoComplete="new-password"></input>
-              <div className="invalid-feedback">Por favor, introduzca un número</div>
-            </div>
+              <div className="form-group">
+                <label htmlFor="municipality" className="lb-lg">Municipio</label>
+                <input id="municipality" type="text" 
+                  className="form-control" required="" autoComplete="on"
+                  value={this.state.municipality}
+                  onChange={e => this.setMunicipality(e.target.value)}
+                />
+                <div className="invalid-feedback">Por favor, introduzca un municipio correcto</div>
+              </div>
 
-            <button type="submit" className="btn btn-success btn-lg float-right" id="btnLogin">Calcular</button>
+              <div className="form-group">
+                <label>Días hábiles</label>
+                <input id="workDays" type="text" 
+                  className="form-control" required="" autoComplete="on"
+                  value={this.state.workDays}
+                  onChange={e => this.setworkDays(e.target.value)}
+                />
+                <div className="invalid-feedback">Por favor, introduzca un número</div>
+              </div>
 
-          </form>
+              <button type="submit" className="btn btn-success btn-lg float-right" id="btnLogin">Calcular</button>
+
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default DeadlineCalculator;
