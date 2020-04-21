9484995427429928f0299a40c7ce1373548b0e25
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-04-21 08:07:49 +0200

    Refactorizada la busqueda de municipios a un scope

--- app/controllers/api/municipality_controller.rb
+++ app/controllers/api/municipality_controller.rb
@@ -1,12 +1,5 @@
 class Api::MunicipalityController < ApplicationController
-
-  NAME_FIELD_WITHOUT_SPACES = "LOWER(REPLACE(`name`, ' ', ''))"
-
   def search()
-    search_expression = ".*#{params['name'].downcase}.*"
-    
-    @municipalities = Municipality.where(
-      "#{NAME_FIELD_WITHOUT_SPACES} REGEXP ?", search_expression
-    )
+    @municipalities = Municipality.similar_to(params['name'])
   end
 end
--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -7,4 +7,11 @@ class Municipality < ApplicationRecord
   validates :code, presence: true, uniqueness: true, length: {minimum: 5, maximum: 5}, allow_blank: false
   validates :name, presence: true
 
+  NAME_FIELD_WITHOUT_SPACES = "LOWER(REPLACE(`name`, ' ', ''))"
+
+  scope :similar_to, ->(name) {
+    where(
+      "#{NAME_FIELD_WITHOUT_SPACES} LIKE ?", "%#{name}%"
+    )
+  }
 end
