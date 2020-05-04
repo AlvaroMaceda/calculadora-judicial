bfcea6ef8d045546f4a8bca05555b7b640d6a4cc
Alvaro <alvaro@alvaronaceda.es>
2020-05-04 14:21:28 +0200

    Working...

---
+++ app/controllers/admin/autonomous_community_import_controller.rb
@@ -0,0 +1,10 @@
+class Admin::AutonomousCommunityImportController < ApplicationController
+
+  def new
+    puts 'ejecutando new'
+  end
+
+  def create
+  end
+
+end
---
+++ app/helpers/autonomous_community_import_helper.rb
@@ -0,0 +1,2 @@
+module AutonomousCommunityImportHelper
+end
