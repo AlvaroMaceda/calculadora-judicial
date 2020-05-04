6b376f059b05a0c17463194be0aa9c5c2c4862a3
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-05-04 22:54:48 +0200

    Primeras pruebas de importación de AC

--- lib/autonomous_community_importer.rb
+++ lib/autonomous_community_importer.rb
@@ -7,18 +7,25 @@ class AutonomousCommunityImporter
     end
 
     def importCSV(csv_file)
-        CSV.new(csv_file, :headers => true) do |row|
-            curated_row = {
-                country_id: @country.id+0,
-                name: row['name'],
-                code: row['code']
-            }
-            puts curated_row
-            ac = AutonomousCommunity.create(curated_row.to_h)
-            # ac.errors.full_messages.each do |message|
-            #     # do stuff for each error
-            # end
+        csv = CSV.new(csv_file, :headers => true) 
+        csv.each do |row|
+            create_ac row
         end
     end
 
+    private
+
+    def create_ac(row_data)
+        curated_row = {
+            country_id: @country.id+0,
+            name: row_data['name'],
+            code: row_data['code']
+        }
+        ac = AutonomousCommunity.create(curated_row.to_h)
+        # puts ac.valid?
+        # ac.errors.full_messages.each do |message|
+        #     # do stuff for each error
+        #     puts message
+        # end
+    end
 end
--- spec/lib/autonomous_community_importer_spec.rb
+++ spec/lib/autonomous_community_importer_spec.rb
@@ -1,43 +1,40 @@
 require 'rails_helper'
 
-CSV_PATH = File.join(__dir__,'csv','autonomous_communities')
-def csv_example(file_name)
-    File.open( File.join(CSV_PATH,file_name+'.csv') , "r")
+def ac_data(ac)
+    { code: ac.code, name: ac.name, country: ac.country.id}
 end
 
 describe DeadlineCalculator do
 
-    let(:importer) { AutonomousCommunityImporter.new(create(:country)) }
+    let(:country) { create(:country) }
+    let(:importer) { AutonomousCommunityImporter.new(country) }
 
     it 'class exists' do
         expect { AutonomousCommunityImporter }.not_to raise_error
     end
 
     it 'imports autonomous communities' do
-        
-        
-        # This does not work
+       
         csv_data = <<~HEREDOC
             code,name
-            "01","Autonomous Communitiy 1"
-            "02","Autonomous Communitiy 2"
-            "03","Autonomous Communitiy 3
+            "01","Autonomous Community 1"
+            "02","Autonomous Community 2"
+            "03","Autonomous Community 3"
         HEREDOC
+        expected = [
+            {code: '01', name: 'Autonomous Community 1', country: country.id},
+            {code: '02', name: 'Autonomous Community 2', country: country.id},
+            {code: '03', name: 'Autonomous Community 3', country: country.id}
+        ]
+
         csv = StringIO.new(csv_data)
-        puts csv.read
         importer.importCSV csv
 
-        # This works
-        # csv = csv_example('correct_example_1')
-        # importer.importCSV csv
-        
-        puts AutonomousCommunity.all.count
-        AutonomousCommunity.all.each do |ac|
-            puts "code: #{ac.code} name: #{ac.name}"
-        end
+        retrieved = AutonomousCommunity.all.to_a.map { |ac| ac_data(ac) }
+        expect(retrieved).to match_array(expected)
     end
 
-    context 'malformed csv' do
+    context 'errors in csv' do
     end
 
 end
