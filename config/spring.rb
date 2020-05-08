1bd77255dcd14ee9c49a3205ade0ab00c19b74a1
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-05-08 19:36:38 +0200

    Modificada la configuracion de RSpec y Spring para que ejecuten los tests aleatoriamente

--- config/spring.rb
+++ config/spring.rb
@@ -4,3 +4,13 @@ Spring.watch(
   "tmp/restart.txt",
   "tmp/caching-dev.txt"
 )
+
+# Change seed each run 
+# See: https://github.com/rails/spring/issues/113
+Spring.after_fork do
+  if Rails.env == 'test'
+    RSpec.configure do |config|
+      config.seed = srand % 0xFFFF unless ARGV.any? { |arg| arg =~ /seed/ }
+    end
+  end
+end
