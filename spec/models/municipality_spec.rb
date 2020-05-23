9afc0cd38cda2c0382ebd691b45a238a1187bc50
Alvaro <alvaro@alvaronaceda.es>
2020-05-23 13:41:48 +0200

    Añadido test para buscar sin tener en cuenta los acentos

--- spec/models/municipality_spec.rb
+++ spec/models/municipality_spec.rb
@@ -64,4 +64,40 @@ describe Municipality, type: :model do
         
     end
 
+    context 'similar_to scope' do
+
+        before(:each) do
+            @ac1 = create(:autonomous_community)            
+            @ac2 = create(:autonomous_community)
+
+            @alcala_data = {name: "Alcalá", code: "ES50001", autonomous_community: @ac1 }
+            @calahorra_data = {name: "Calahorra", code: "ES50002", autonomous_community: @ac1}
+            @calcatta_data = {name: "Calcatta", code: "ES50003", autonomous_community: @ac1}
+            @la_costa_este_data = {name: "La Costa Este", code: "ES80001", autonomous_community: @ac2}
+            @sal_calada_data = {name: "Sal calada Pèmera", code: "ES80002", autonomous_community: @ac2}
+            @valencia_de_alcantara_data = {name: "Valencia de Alcántara", code: "ES80003", autonomous_community: @ac2}
+    
+            @alcala = create(:municipality, @alcala_data )
+            @calahorra = create(:municipality, @calahorra_data )
+            @calcatta = create(:municipality, @calcatta_data )
+            @la_costa_este = create(:municipality, @la_costa_este_data )
+            @sal_calada = create(:municipality, @sal_calada_data )
+        end
+
+        it 'searches ignoring accents' do
+            search_text = 'cala'
+            expected = [
+                @alcala, @calahorra, @sal_calada
+            ]
+
+            response = Municipality.similar_to(search_text).to_a
+            response.each do |m|
+                puts m.name
+            end
+            
+            expect(response).to match_array(expected)
+        end
+
+    end
+
 end
