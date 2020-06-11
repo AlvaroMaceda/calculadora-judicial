d2538a3195530bfd1e92bc59ea1107242d6b2243
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-06-11 20:11:45 +0200

    Primera version de la visualizacion de meses

--- app/javascript/components/calendar.jsx
+++ app/javascript/components/calendar.jsx
@@ -7,10 +7,23 @@ function renderMonths(year, months) {
   return months.map(month => <Month key={year+month} year={year} month={month}/>)
 }
 
+const lalala = `
+Julio 2020       
+lu ma mi ju vi sá do  
+29 30  1  2  3  4  5  
+ 6  7  8  9 10 11 12  
+13 14 15 16 17 18 19  
+20 21 22 23 24 25 26  
+27 28 29 30 31  
+`
+
 function Calendar({year, months}) {
 
   return (
     <React.Fragment>
+      <pre>
+      {lalala}      
+      </pre>
       <h1>OLA K ASE</h1>
       { renderMonths(year, months) }
       <br/>
--- app/javascript/components/calendar_month.jsx
+++ app/javascript/components/calendar_month.jsx
@@ -1,54 +1,104 @@
 
 import React from 'react'
 import * as dateFns from "date-fns"; // Change to moment.js, it seems better for working with locales
+import moment from 'moment'
+import classNames from 'classnames'
+import style from './calendar_month.module.scss'
 //https://til.hashrocket.com/posts/cxd9yl95ip--get-beginning-and-end-of-week-with-momentjs
 //https://stackoverflow.com/questions/17493309/how-do-i-change-the-language-of-moment-js
 
 // https://blog.flowandform.agency/create-a-custom-calendar-in-react-3df1bfd0b728
 
-var date = new Date();
-
-var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+moment.locale('es')
 
 
-function renderDays(year, month) {
+// I don't like this, it abreviates as "lu ma mi ju vi sá do"
+function renderDayNames_i18n() {
 
-  var firstDay = new Date(year, month-1, 1);
-
-  const dateFormat = "dddd";
+  const dateFormat = "dd"
   const days = [];  
-  let startDate = dateFns.startOfWeek(firstDay);
-  console.log(`firstDay: ${firstDay}`)
-  console.log(`startDate:${startDate}`)
+  const currentDate = moment().startOf('week')
   
   for (let i = 0; i < 7; i++) {
-    console.log(dateFns.addDays(startDate, i))
     days.push(
       <div className="col col-center" key={i}>
-        {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        { currentDate.format(dateFormat) }
       </div>
-    );
+    )
+    currentDate.add(1, 'd')
   }
 
-  return <div className="days row">{days}</div>;
+  return <div className="days row">{days}</div>
+} // renderDayNames_i18n
 
-} // renderDays
 
-function renderCells() {
+function renderDayNames() {
 
-}
+  const days = ['L','M','X','J','V','S','D']
+  let cells = []
+
+  days.forEach( (day, i) => {
+    cells.push(
+      <div className={classNames(style.col,style.col_center)} key={i}>
+        { day }
+      </div>
+    )
+  })
+
+  return <div className={classNames(style.days,style.row)}>{cells}</div>
+} // renderDayNames
+
+function renderCells(year, month) {
+  const monthStart = moment([year,month,1])
+  const monthEnd = moment(monthStart).endOf('month')
+
+  const startDate = moment(monthStart).startOf('week')
+  const endDate = moment(monthEnd).endOf('week')
+
+  const rows = []
+
+  let days = []
+  let day = startDate
+
+  while (day <= endDate) {
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div
+          className={classNames(style.col,style.cell,
+              !day.isSame(monthStart,'month') ? style.disabled : style.enabled
+          )}
+          key={day}
+        >
+          <span className={style.number}>{day.format('D')}</span>
+        </div>
+      )
+      day.add(1, 'd')
+    }
+    rows.push(
+      <div className={style.row} key={day}>
+        {days}
+      </div>
+    )
+    days = []
+  }
+  return <div className={style.body}>{rows}</div>
+} // renderCells
 
 function CalendarMonth({year, month}) {
 
+  month = month -1 // Adjust to javascript months
+  const monthDate = moment([year,month,1])
+  const monthLabel = `${monthDate.format('MMMM')} ${monthDate.format('Y')}`
+
   return (
-    <div className="month">
-      <h2>Mes {month} del año {year}</h2>
-      { renderDays(year, month) }
-      { renderCells() }
+    <div className={style.calendar}>
+      <div className={style.header}>{monthLabel}</div>
+      { renderDayNames() }
+      { renderCells(year, month) }
     </div>
 
   )
 
 }
 
-export default CalendarMonth;
+export default CalendarMonth
