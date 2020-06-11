4a60cf44a174c3c69c76d5402edf6aac9e846c26
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-06-11 23:11:05 +0200

    Eliminado autocomplete. Pasado calendar a flex

--- app/javascript/components/calendar.jsx
+++ app/javascript/components/calendar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Month from './calendar_month'
+import style from './calendar.module.scss'
 
 // https://blog.flowandform.agency/create-a-custom-calendar-in-react-3df1bfd0b728
 
@@ -7,28 +8,12 @@ function renderMonths(year, months) {
   return months.map(month => <Month key={year+month} year={year} month={month}/>)
 }
 
-const lalala = `
-Julio 2020       
-lu ma mi ju vi sá do  
-29 30  1  2  3  4  5  
- 6  7  8  9 10 11 12  
-13 14 15 16 17 18 19  
-20 21 22 23 24 25 26  
-27 28 29 30 31  
-`
-
 function Calendar({year, months}) {
 
   return (
-    <React.Fragment>
-      <pre>
-      {lalala}      
-      </pre>
-      <h1>OLA K ASE</h1>
+    <div class={style.calendar}>
       { renderMonths(year, months) }
-      <br/>
-      {year}
-    </React.Fragment>
+    </div>
   )
 
 }
