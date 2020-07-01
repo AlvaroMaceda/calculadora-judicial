a5eef442ddd59ef2129329ee011b3659578baf73
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-07-01 11:11:46 +0200

    Funcionalidad javascript ok en ie11. Errores con estilos

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// For IE 11 support
+import "core-js/stable";
+import 'whatwg-fetch'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
