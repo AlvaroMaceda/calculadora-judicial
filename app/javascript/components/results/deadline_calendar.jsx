9151f89704e9f43a27b28db7c2f58fd859329bd3
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-07-01 12:14:56 +0200

    Ajustes en estilos para IE11

--- app/javascript/components/results/deadline_calendar.jsx
+++ app/javascript/components/results/deadline_calendar.jsx
@@ -47,15 +47,15 @@ const markStyles = {
     background: TERM_BACKGROUND, color: 'white'
   },
   termStart: {
-    // background: TERM_BACKGROUND, color: 'white',
-    border: '2px solid #0075a0',
-    // borderLeft: '5px solid #0075a0',
+    // We use outline and z-index instead of border because IE 11
+    outline: '2px solid #0075a0',
+    zIndex: 9
   },
   termEnd: {
     background: TERM_BACKGROUND, color: 'white',
-    border: '2px solid red',
-    // borderRight: '5px solid red'
-    // borderRight: '5px solid black'
+    // We use outline and z-index instead of border because IE 11
+    outline: '2px solid red',
+    zIndex: 9
   },
 
   ...holidayStyles
